5ea149c3474f9dccf455420ce4ae105f0dced203
Wellington Faustino <well.faustino@hotmail.com>
2023-10-29 22:47:30 -0300

    Inicial, JWT + Autenticação

--- src/web-api/api-application/Configuration/ApiConfig.cs
+++ src/web-api/api-application/Configuration/ApiConfig.cs
@@ -55,29 +55,28 @@ namespace ApiApplication.Configuration
 				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
 				c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
 				{
-					Description = @"JWT Authorization header using the Bearer scheme.
-						Enter 'Bearer' [space] and then your token in the text input below. Example: 'Bearer 12345abcdef'",
+					Description = @"JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and then your token in the text input below. Example: 'Bearer 12345abcdef'",
 					Name = "Authorization",
 					In = ParameterLocation.Header,
 					Type = SecuritySchemeType.ApiKey,
 					Scheme = "Bearer"
 				});
 
-				c.AddSecurityRequirement(new OpenApiSecurityRequirement() {
+				c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+				{
 					{new OpenApiSecurityScheme {
-						Reference = new OpenApiReference
-						  {
-							Type = ReferenceType.SecurityScheme,
-							Id = "Bearer"
-						  },
-						  Scheme = "oauth2",
-						  Name = "Bearer",
-						  In = ParameterLocation.Header,
-
+							Reference = new OpenApiReference 
+							{
+								Type = ReferenceType.SecurityScheme,
+								Id = "Bearer"
+							},
+							Scheme = "oauth2",
+							Name = "Bearer",
+							In = ParameterLocation.Header,
 						},
 						new List<string>()
 					  }
-					});
+				});
 
 			});
 		}
